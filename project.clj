98de78c5b21fa005190787af41d027191b9a2cb0
Michael Baker <michaeltbaker@gmail.com>
2012-03-12 23:46:20 +0000

    Sanity is in check

--- project.clj
+++ project.clj
@@ -1,5 +1,7 @@
 (defproject clojure-webserver "1.0.0-SNAPSHOT"
   :description  "FIXME: write description"
   :dependencies [[org.clojure/clojure "1.3.0"]
-                 [org.clojure/tools.cli "0.2.1"]]
+                 [org.clojure/tools.cli "0.2.1"],
+                 [ring/ring-jetty-adapter "0.3.6"],
+                 [ring/ring-core "1.0.2"]]
   :main         clojure-webserver.core)
--- src/clojure_webserver/core.clj
+++ src/clojure_webserver/core.clj
@@ -1,13 +1,17 @@
 (ns clojure-webserver.core
-  (:use clojure.tools.cli)
+  (:use clojure.tools.cli
+        ring.adapter.jetty)
   (:import java.io.File)
   (:gen-class))
 
 (defn resource-path [relative-path]
   (.. (File. relative-path) (getAbsolutePath)))
 
+(defn make-app [directory]
+  (fn [req] {:status 200 :headers {} :body "Hello World"}))
+
 (defn -main [& args]
   (let [[switches remaining usage] (cli args
        ["-p" "--port" "Start the server on this port." :parse-fn #(Integer. %) :default 5000]
        ["-d" "--directory" "The directory from which files will be served." :default "public"])]
-    ))
+    (run-jetty (make-app (switches :directory)) {:port (switches :port)})))
