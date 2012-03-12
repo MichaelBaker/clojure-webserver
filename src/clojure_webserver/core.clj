783176564f277739a4d5cba7ed3500f5b44d0202
Michael Baker <michaeltbaker@gmail.com>
2012-03-12 23:09:11 +0000

    Parses the resource directory

--- src/clojure_webserver/core.clj
+++ src/clojure_webserver/core.clj
@@ -1,8 +1,13 @@
 (ns clojure-webserver.core
   (:use clojure.tools.cli)
+  (:import java.io.File)
   (:gen-class))
 
+(defn resource-path [relative-path]
+  (.. (File. relative-path) (getAbsolutePath)))
+
 (defn -main [& args]
-  (println (cli args
-       ["-p" "--port" "Start the server on this port." :parse-fn #(Integer. %)]
-       ["-d" "--directory" "The directory from which files will be served."])))
+  (let [[switches remaining usage] (cli args
+       ["-p" "--port" "Start the server on this port." :parse-fn #(Integer. %) :default 5000]
+       ["-d" "--directory" "The directory from which files will be served." :default "public"])]
+    ))
