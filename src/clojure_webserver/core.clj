384f692bd1d1ba207c610b081a2325cd87e230f5
Michael Baker <michaeltbaker@gmail.com>
2012-03-13 21:41:55 +0000

    Starting over

--- src/clojure_webserver/core.clj
+++ src/clojure_webserver/core.clj
@@ -1,17 +1,8 @@
 (ns clojure-webserver.core
-  (:use clojure.tools.cli
-        ring.adapter.jetty)
-  (:import java.io.File)
+  (:use clojure.tools.cli)
   (:gen-class))
-
-(defn resource-path [relative-path]
-  (.. (File. relative-path) (getAbsolutePath)))
-
-(defn make-app [directory]
-  (fn [req] {:status 200 :headers {} :body "Hello World"}))
 
 (defn -main [& args]
   (let [[switches remaining usage] (cli args
        ["-p" "--port" "Start the server on this port." :parse-fn #(Integer. %) :default 5000]
-       ["-d" "--directory" "The directory from which files will be served." :default "public"])]
-    (run-jetty (make-app (switches :directory)) {:port (switches :port)})))
+       ["-d" "--directory" "The directory from which files will be served." :default "public"])]))
