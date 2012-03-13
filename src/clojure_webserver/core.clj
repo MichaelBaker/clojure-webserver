56d6a4ff48e2df1a1e7eb91cbd0c6e54e8467be6
Michael Baker <michaeltbaker@gmail.com>
2012-03-13 22:04:02 +0000

    Accepting connections

--- src/clojure_webserver/core.clj
+++ src/clojure_webserver/core.clj
@@ -1,8 +1,21 @@
 (ns clojure-webserver.core
   (:use clojure.tools.cli)
+  (:import java.net.ServerSocket)
   (:gen-class))
+
+(defn worker [socket]
+  (println (str "Got a socket:" socket))
+  (.close socket))
+
+(defn accept-connection [server]
+  (Thread. (worker (.accept server)))
+  (recur server))
+
+(defn start-server [port]
+  (accept-connection (ServerSocket. port)))
 
 (defn -main [& args]
   (let [[switches remaining usage] (cli args
        ["-p" "--port" "Start the server on this port." :parse-fn #(Integer. %) :default 5000]
-       ["-d" "--directory" "The directory from which files will be served." :default "public"])]))
+       ["-d" "--directory" "The directory from which files will be served." :default "public"])]
+    (start-server (switches :port))))
