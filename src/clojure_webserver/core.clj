9b16d286b05e692504749b4d7d321f6f180c2cd0
Michael Baker <michaeltbaker@gmail.com>
2012-03-15 19:17:58 +0000

    Speeds up the requests per second by using a thread pool

--- src/clojure_webserver/core.clj
+++ src/clojure_webserver/core.clj
@@ -1,28 +1,27 @@
 (ns clojure-webserver.core
   (:import java.net.ServerSocket)
+  (:import java.util.concurrent.Executors)
   (:use [clojure.java.io :only [file]])
   (:use clojure.tools.cli)
   (:use clojure-webserver.http-handler)
   (:use clojure-webserver.request-handler)
   (:gen-class))
 
-(def handler)
+(defn accept-connection [server request-handler]
+  (let [thread-pool (Executors/newFixedThreadPool 4)]
+    (while true (.execute thread-pool
+                          (http-handler (.accept server)
+                                        request-handler)))))
 
-(defn accept-connection [server]
-  (let [socket (.accept server)]
-    (println (str "[connect] " socket))
-    (.. (Thread. (http-handler socket handler))
-        (start))
-    (recur server)))
-
-(defn start-server [port]
+(defn start-server [port request-handler]
   (println "[start] Server is awaiting connections on port " port)
   (println "[start] Press ^C to quit")
-  (accept-connection (ServerSocket. port)))
+  (accept-connection (ServerSocket. port) request-handler))
 
 (defn -main [& args]
   (let [[switches remaining usage] (cli args
-       ["-p" "--port" "Start the server on this port." :parse-fn #(Integer. %) :default 5000]
-       ["-d" "--directory" "The directory from which files will be served." :default "public"])]
-    (def handler (request-handler (file (switches :directory))))
-    (start-server (switches :port))))
+        ["-p" "--port" "Start the server on this port." :parse-fn #(Integer. %) :default 5000]
+        ["-d" "--directory" "The directory from which files will be served." :default "public"])
+        handler (request-handler (file (switches :directory)))
+        port    (switches :port)]
+    (start-server port handler)))
