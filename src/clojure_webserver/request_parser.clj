cbff1466a6c65793214f5395e6a5301049cc4941
Michael Baker <michaeltbaker@gmail.com>
2012-03-15 20:11:45 +0000

    Removes uneeded functionality from the request parser

--- src/clojure_webserver/request_parser.clj
+++ src/clojure_webserver/request_parser.clj
@@ -2,15 +2,10 @@
   (:import java.io.BufferedReader)
   (:use [clojure.string :only [split-lines split]]))
 
-(defn -get-request-line [{[line & rest] :remaining}]
+(defn -parse-request-line [line]
   (let [[method uri version] (split line #"\s")]
     {:method method :uri uri :version version :remaining rest}))
 
-(defn -get-headers [request]
-  (dissoc request :remaining))
-
 (defn parse-request [reader]
-  (let [input (line-seq (BufferedReader. reader))]
-    (-> {:remaining input}
-        (-get-request-line)
-        (-get-headers))))
+  (let [[first & rest] (line-seq (BufferedReader. reader))]
+    (-parse-request-line first)))
